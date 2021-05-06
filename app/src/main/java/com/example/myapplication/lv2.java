561db963d0060b08e019ee957dcdd74204a588ec
EgorLubyshev <egorlubyshev25@gmail.com>
2021-05-06 20:50:15 +0300

    comm it2

--- app/src/main/java/com/example/myapplication/lv2.java
+++ app/src/main/java/com/example/myapplication/lv2.java
@@ -1,10 +1,12 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class lv2 extends AppCompatActivity {
+    public TextView c1, c2, c3, c4, a1, a2, a3, a4, textView, check;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
