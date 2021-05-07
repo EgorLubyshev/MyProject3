2d080912175204ac164c0902db5b56a6206811fe
EgorLubyshev <egorlubyshev25@gmail.com>
2021-05-07 12:04:08 +0300

    commit5

--- app/src/main/java/com/example/myapplication/lv3.java
+++ app/src/main/java/com/example/myapplication/lv3.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.view.Display;
@@ -10,26 +11,28 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class lv2 extends AppCompatActivity {
-    public TextView c1, c2, c3, a1, a2, a3, textView, check;
+public class lv3 extends AppCompatActivity {
+
+    public TextView c1, c2, c3, c4, a1, a2, a3, a4, textView, check;
     Display display;
     Point size;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lv2);
+        setContentView(R.layout.lv3);
 
-        textView = findViewById(R.id.text_view1);
-        check = findViewById(R.id.check);
+        textView=findViewById(R.id.text_view1);
+        check=findViewById(R.id.check);
 
-        c1 = findViewById(R.id.c1);
-        c2 = findViewById(R.id.c2);
-        c3 = findViewById(R.id.c3);
+        c1=findViewById(R.id.c1);
+        c2=findViewById(R.id.c2);
+        c3=findViewById(R.id.c3);
+        c4=findViewById(R.id.c4);
 
-        a1 = findViewById(R.id.answer1);
-        a2 = findViewById(R.id.answer2);
-        a3 = findViewById(R.id.answer3);
+        a1=findViewById(R.id.answer1);
+        a2=findViewById(R.id.answer2);
+        a3=findViewById(R.id.answer3);
+        a4=findViewById(R.id.answer4);
 
         display = getWindowManager().getDefaultDisplay();
         size = new Point();
@@ -37,12 +40,15 @@ public class lv2 extends AppCompatActivity {
         int width = size.x;
         int height = size.y;
 
-        a1.setHeight(height / 9);
-        a2.setHeight(height / 9);
-        a3.setHeight(height / 9);
+        a1.setHeight(height/12);
+        a2.setHeight(height/12);
+        a3.setHeight(height/12);
+        a4.setHeight(height/12);
         a1.setWidth(width / 3);
         a2.setWidth(width / 3);
         a3.setWidth(width / 3);
+        a4.setWidth(width / 3);
+
 
 
         c1.setOnClickListener(new View.OnClickListener() {
@@ -63,24 +69,38 @@ public class lv2 extends AppCompatActivity {
                 c(c3);
             }
         });
-        a1.setOnClickListener(new View.OnClickListener() {
+        c4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                c(c4);
+            }
+        });
+
+        a1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
                 ans(a1);
             }
         });
         a2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 ans(a2);
             }
         });
         a3.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 ans(a3);
             }
         });
+        a4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                ans(a4);
+            }
+        });
+
         check.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -89,48 +109,52 @@ public class lv2 extends AppCompatActivity {
         });
 
     }
-
-    void c(TextView c) {
-        if (a1.getText().equals("")) {
+    void c(TextView c){
+        if (a1.getText().equals("")){
             a1.setText(c.getText());
             c.setVisibility(View.INVISIBLE);
-        } else {
-            if (a2.getText().equals("")) {
+        }else {
+            if(a2.getText().equals("")){
                 a2.setText(c.getText());
                 c.setVisibility(View.INVISIBLE);
-            } else {
-                if (a3.getText().equals("")) {
+            }else {
+                if (a3.getText().equals("")){
                     a3.setText(c.getText());
                     c.setVisibility(View.INVISIBLE);
+                }else {
+                    if(a4.getText().equals("")) {
+                        a4.setText(c.getText());
+                        c.setVisibility(View.INVISIBLE);
+                    }
                 }
             }
         }
     }
-
-    void ans(TextView answer) {
-        if (answer.getText().equals(c1.getText())) {
+    void ans( TextView answer){
+        if (answer.getText().equals(c1.getText())){
             c1.setVisibility(View.VISIBLE);
             answer.setText("");
         }
-        if (answer.getText().equals(c2.getText())) {
+        if (answer.getText().equals(c2.getText())){
             c2.setVisibility(View.VISIBLE);
             answer.setText("");
         }
-        if (answer.getText().equals(c3.getText())) {
+        if (answer.getText().equals(c3.getText())){
             c3.setVisibility(View.VISIBLE);
             answer.setText("");
         }
-
+        if (answer.getText().equals(c4.getText())){
+            c4.setVisibility(View.VISIBLE);
+            answer.setText("");
+        }
     }
-
-    void check() {
-        if ((a1.getText().equals("a=3") || a1.getText().equals("b=2") )&& (a2.getText().equals("a=3") ||
-                a2.getText().equals("b=2")) &&
-                a3.getText().equals("print(a+b)")) {
+    void check(){
+        if (a1.getText().equals("a=3") && a2.getText().equals("for i in range(1,4):") &&
+                a3.getText().equals("a+=i") && a4.getText().equals("print(a)")){
             Toast.makeText(this, "все верно!", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(getApplicationContext(), lv3.class);
+            Intent intent = new Intent(getApplicationContext(), lv2.class);
             startActivity(intent);
-        } else {
+        }else {
             Toast.makeText(this, "Ошибка!", Toast.LENGTH_SHORT).show();
         }
     }
