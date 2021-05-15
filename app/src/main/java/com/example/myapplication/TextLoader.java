7b71c2a99f2afb5c15a697712f16bc6374b9f37a
Kugeki <54151036+Kugeki@users.noreply.github.com>
2021-05-15 15:00:08 +0300

    commit7

---
+++ app/src/main/java/com/example/myapplication/TextLoader.java
@@ -0,0 +1,97 @@
+package com.example.myapplication;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+class TextLoader extends AsyncTask<String, Integer, Answer> {
+    TextView textView;
+    static String str="";
+
+    public void TextViewLoader(TextView textView, String str) {
+
+    }
+
+    @Override
+    protected Answer doInBackground(String... strings) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(strings[0])
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        UserServers userServers = retrofit.create(UserServers.class);
+        Call<Answer> call = userServers.getUsers();
+
+        try {
+            Response<Answer> response = call.execute();
+            return response.body();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    @Override
+    protected void onPostExecute(Answer answer) {
+        if (answer != null) {
+            textView.setText(answer.getUsers());
+        } else {
+            textView.setText("Ошибка");
+        }
+
+
+    }
+
+
+    interface UserServers {
+
+        @GET("get_user.php")
+        Call<Answer> getUsers();
+
+        @GET("set_user.php")
+        Call<Answer> setUsers(
+                @Query("login") String login,
+                @Query("password") String pass,
+                @Query("mail") String mail
+        );
+    }
+
+
+}
+
+class Answer {
+    boolean status;
+    ArrayList<User> data;
+
+    String getUsers() {
+        String res = "";
+        for (User user : data) {
+            res += user.toString() + "\n";
+
+        }
+        return res;
+    }
+}
+
+class User {
+    int id;
+    String login, password, mail;
+
+    @NonNull
+    @Override
+    public String toString() {
+        return id + ", " + login + ", " + password + ", " + mail;
+    }
+}
