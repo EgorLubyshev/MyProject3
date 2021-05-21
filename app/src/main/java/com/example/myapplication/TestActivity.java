0530430486ae9cfc1e2878f45e79f9e65640a5e7
Kugeki <54151036+Kugeki@users.noreply.github.com>
2021-05-21 18:34:43 +0300

    commit2

--- app/src/main/java/com/example/myapplication/TestActivity.java
+++ app/src/main/java/com/example/myapplication/TestActivity.java
@@ -10,21 +10,24 @@ import java.util.ArrayList;
 
 public class TestActivity extends AppCompatActivity {
     ArrayList<Task> tasks = new ArrayList<>();
-    TextView textView1, textView2, textView3, textView4;
+    TextView textView1, textView2, textView3, textView4, textView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView1 = findViewById(R.id.c1);//, textView2, textView3, textView4
-
+        textView1 = findViewById(R.id.c1);
+        textView2 = findViewById(R.id.c2);
+        textView3 = findViewById(R.id.c3);
+        textView4 = findViewById(R.id.c4);
+        textView = findViewById(R.id.text_view1);
         TasksLoader tasksLoader = new TasksLoader();
         tasksLoader.setOnPostExecute(new TasksLoader.OnPostExecute() {
             @Override
             public void doOnPostExecute(TaskAnswer answer) {
                 tasks = answer.data;
-                
+
                 setTask(tasks.get(0));
             }
         });
@@ -34,5 +37,16 @@ public class TestActivity extends AppCompatActivity {
 
     void setTask(Task task){
         textView1.setText(task.variants.get(0));
+        textView2.setText(task.variants.get(1));
+        textView3.setText(task.variants.get(2));
+        textView4.setText(task.variants.get(3));
+        textView.setText(task.text);
+
+        if (task.type==1){
+            setContentView(R.layout.activity_main);
+        }
+        if (task.type==2){
+            setContentView(R.layout.activity_main);
+        }
     }
 }
